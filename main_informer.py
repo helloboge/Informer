4a850b53154972c68586de0d3d4808103d64d6e1
tangbo <2673116770@qq.com>
2023-04-14 22:19:45 +0800

    修改gpu 参数

--- main_informer.py
+++ main_informer.py
@@ -52,7 +52,7 @@ parser.add_argument('--num_workers', type=int, default=0, help='data loader num 
 parser.add_argument('--itr', type=int, default=2, help='experiments times')
 parser.add_argument('--train_epochs', type=int, default=200, help='train epochs')
 parser.add_argument('--batch_size', type=int, default=64, help='batch size of train input data')
-parser.add_argument('--patience', type=int, default=3, help='early stopping patience')
+parser.add_argument('--patience', type=int, default=7, help='early stopping patience')
 parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
 parser.add_argument('--des', type=str, default='test',help='exp description')
 parser.add_argument('--loss', type=str, default='mse',help='loss function')
