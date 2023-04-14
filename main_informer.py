c9a8c6ece11bf1409ceff7c2406374387a3b9c17
tangbo <2673116770@qq.com>
2023-04-14 19:40:38 +0800

    修改main_gpu 参数

--- main_informer.py
+++ main_informer.py
@@ -62,7 +62,8 @@ parser.add_argument('--inverse', action='store_true', help='inverse output data'
 
 parser.add_argument('--use_gpu', type=bool, default=True, help='use gpu')
 parser.add_argument('--gpu', type=int, default=0, help='gpu')
-parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+# parser.add_argument('--use_multi_gpu', action='store_true', help='use multiple gpus', default=False)
+parser.add_argument('--use_multi_gpu', action='store_false', help='use multiple gpus', default=True)
 parser.add_argument('--devices', type=str, default='0,1,2,3',help='device ids of multile gpus')
 
 args = parser.parse_args()
